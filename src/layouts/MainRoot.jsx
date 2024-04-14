444bed9707b903e2d1455be146eb98186557fbf6
mryaxyobek <pubgn9642@gmail.com>
2024-04-14 17:20:12 +0500

    add toast container to main root

--- src/layouts/MainRoot.jsx
+++ src/layouts/MainRoot.jsx
@@ -4,14 +4,32 @@ import { Outlet } from "react-router-dom";
 // components
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+
+// toast
+import { ToastContainer, Zoom } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const MainRoot = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* <Header /> */}
+    <div className="flex flex-col min-h-screen font-fira_sans">
+      <Header />
+
       <main className="grow">
         <Outlet />
       </main>
+
       {/* <Footer /> */}
+
+      {/* notification */}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3500}
+        hideProgressBar={true}
+        closeOnClick={true}
+        pauseOnHover={true}
+        draggable={false}
+        theme={"light "}
+        transition={Zoom}
+      />
     </div>
   );
 };
