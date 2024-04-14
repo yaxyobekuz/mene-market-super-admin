4694d3b19083d80223317815d3ba8ce7a80b9fe3
mryaxyobek <pubgn9642@gmail.com>
2024-04-15 01:08:58 +0500

    add tab buttons data

---
+++ src/data/data.js
@@ -0,0 +1,57 @@
+export const tabButtons = [
+  {
+    id: 0,
+    body: "Barchasi",
+    name: "",
+  },
+  {
+    id: 1,
+    body: "Aksessuarlar",
+    name: "/accessories",
+  },
+  {
+    id: 2,
+    body: "Elektronika",
+    name: "/electronics",
+  },
+  {
+    id: 3,
+    body: "Mashinalar",
+    name: "/cars",
+  },
+  {
+    id: 4,
+    body: "Gullar",
+    name: "/flowers",
+  },
+  {
+    id: 5,
+    body: "Kompyuterlar",
+    name: "/computers",
+  },
+  {
+    id: 6,
+    body: "Parfumeriya",
+    name: "/parfumery",
+  },
+  {
+    id: 7,
+    body: "Telefonlar",
+    name: "/phones",
+  },
+  {
+    id: 8,
+    body: "Mebellar",
+    name: "/furniture",
+  },
+  {
+    id: 9,
+    body: "Oshxona jihozlari",
+    name: "/kitchen-appliances",
+  },
+  {
+    id: 10,
+    body: "O'yinchoqlar",
+    name: "/toys",
+  },
+];
