838a65239824e82f5c0c1cb12d3b50113a948625
mryaxyobek <pubgn9642@gmail.com>
2024-04-14 17:18:22 +0500

    add brand colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          creamy: {
+            400: "#FEF6EB",
+            500: "#FAEED1",
+            600: "#DED0B6",
+          },
+
+          dark: {
+            800: "#292524",
+          },
+        },
+      },
+
+      // font
+      fontFamily: {
+        "fira-sans": "Fira sans, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
