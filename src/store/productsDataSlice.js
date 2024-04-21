11e8527ecbd8d4479c9b2a6a4b79014b7cb576e7
mryaxyobek <pubgn9642@gmail.com>
2024-04-21 20:43:27 +0500

    add filteredproductsdata and addProductToProductsData functions to productdataslice.js

--- src/store/productsDataSlice.js
+++ src/store/productsDataSlice.js
@@ -12,9 +12,20 @@ export const productsDataSlice = createSlice({
     setProductsData: (state, action) => {
       state.productsData = action.payload;
     },
+
+    filterProductsData: (state, action) => {
+      state.productsData = state.productsData.filter(
+        (product) => product.productId !== action.payload
+      );
+    },
+
+    addProductToProductsData: (state, action) => {
+      state.productsData.push(action.payload);
+    },
   },
 });
 
-export const { setProductsData } = productsDataSlice.actions;
+export const { setProductsData, filterProductsData, addProductToProductsData } =
+  productsDataSlice.actions;
 
 export default productsDataSlice.reducer;
