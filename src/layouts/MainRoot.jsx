2499bc259948ab8ead4673446bd9af7eca7021f0
mryaxyobek <pubgn9642@gmail.com>
2024-04-21 20:55:33 +0500

    add scroll to top function to mainroot.jsx

--- src/layouts/MainRoot.jsx
+++ src/layouts/MainRoot.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 // components
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const MainRoot = () => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
     <div className="flex flex-col min-h-screen font-fira_sans">
       <Header />
