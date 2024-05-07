4b1414a8b79e7afc7906fb5dd4a4a0a943c2df74
mryaxyobek <pubgn9642@gmail.com>
2024-05-07 23:43:58 +0500

    add pagination component

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// images
+import arrowRightImg from "../assets/images/arrow-right.svg";
+const Pagination = ({ currentIndex, lastIndex, link }) => {
+  const index = {
+    current: Number(currentIndex),
+    last: Number(lastIndex),
+  };
+
+  return (
+    <div className="flex items-center justify-center gap-5">
+      {/* previous */}
+      {index.current > 1 ? (
+        <Link
+          aria-label="Next page"
+          to={link + (index.current - 1)}
+          className="rotate-180 border-2 border-brand-dark-800 rounded-full p-2"
+        >
+          <img
+            width={24}
+            height={24}
+            src={arrowRightImg}
+            alt="arrow left icon"
+            className="size-6"
+          />
+        </Link>
+      ) : (
+        <span className="opacity-70 rotate-180 border-2 border-brand-dark-800 rounded-full p-2">
+          <img
+            width={24}
+            height={24}
+            src={arrowRightImg}
+            alt="arrow left icon"
+            className="size-6"
+          />
+        </span>
+      )}
+
+      {/* page number */}
+      <span className="min-w-20 text-center text-lg">
+        Sahifa {index.current}
+      </span>
+
+      {/* next */}
+      {index.last > index.current ? (
+        <Link
+          aria-label="Next page"
+          to={link + (index.current + 1)}
+          className="border-2 border-brand-dark-800 rounded-full p-2"
+        >
+          <img
+            width={24}
+            height={24}
+            src={arrowRightImg}
+            alt="arrow right icon"
+            className="size-6"
+          />
+        </Link>
+      ) : (
+        <span className="opacity-70 border-2 border-brand-dark-800 rounded-full p-2">
+          <img
+            width={24}
+            height={24}
+            src={arrowRightImg}
+            alt="arrow right icon"
+            className="size-6"
+          />
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Pagination;
