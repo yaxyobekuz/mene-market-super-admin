281d2c0518d493d64cfa802a9af3a960b0885b67
yaxyobekb <pubgn9642@gmail.com>
2024-05-20 13:58:01 +0500

    add no data component

---
+++ src/components/NoData.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+// images
+import noDataImg from "../assets/images/no-data.svg";
+import { Link } from "react-router-dom";
+const NoData = ({ title }) => {
+  return (
+    <div className="flex items-center justify-center xs:py-4 sm:py-6 md:py-7">
+      <div className="flex flex-col items-center md:gap-3 md:flex-row">
+        <img
+          width={320}
+          height={320}
+          src={noDataImg}
+          alt="no data image"
+          className="w-8/12 xs:size-64 sm:size-72 md:size-80"
+        />
+
+        {/* main content */}
+        <div className="flex flex-col items-center text-center gap-2.5 max-w-96 md:items-start md:text-start">
+          <h2>
+            {title ? (
+              title
+            ) : (
+              <span>
+                Xatolik! <br /> Ma'lumotlar mavud emas!
+              </span>
+            )}
+          </h2>
+          <p>Ushbu sahifadada hech qanday ma'lumot mavjud emas.</p>
+          <div className="flex flex-col gap-3.5 w-full xs:w-auto xs:flex-row">
+            <Link
+              to={-1}
+              className="   py-2.5 px-3 rounded-xl border-2 border-brand-dark-800"
+            >
+              Ortga qaytish
+            </Link>
+            <Link to="/" className="main-btn">
+              Bosh sahifa
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoData;
