1de2066ffc12967edbed05aab8034acc30b23732
yaxyobekb <pubgn9642@gmail.com>
2024-05-22 21:11:54 +0500

    add products page full content and fix some bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,85 +36,55 @@ const Header = () => {
         </div>
 
         {/* nav */}
-        <nav className="overflow-x-auto hidden-scroll p-1 bg-brand-dark-800/5 rounded-2xl xs:p-1.5">
-          <ul className="flex justify-between gap-2 w-full sm:gap-5">
+        <nav className="main-nav">
+          <ul className="main-nav-links-wrapper">
             <li>
-              <NavLink
-                to="/"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/" className="main-nav-link">
                 Bosh sahifa
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/products"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/products" className="main-nav-link">
                 Mahsulotlar
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/reviews"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/reviews" className="main-nav-link">
                 Izohlar
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/users"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/users" className="main-nav-link">
                 Foydalanuvchilar
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/product-requests"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/product-requests" className="main-nav-link">
                 So'rovlar
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/donation-box"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/donation-box" className="main-nav-link">
                 Hayriya qutisi
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/appeals"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/appeals" className="main-nav-link">
                 Murojaatlar
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/payments"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/payments" className="main-nav-link">
                 To'lovlar
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/contests"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/contests" className="main-nav-link">
                 Konkurslar
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/news"
-                className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
-              >
+              <NavLink to="/news" className="main-nav-link">
                 Yangiliklar
               </NavLink>
             </li>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink, useParams } from "react-router-dom";
 
 // axios
-import axios from "../axios/axios";
+import axiosInstance from "../axios/axiosInstance";
 
 // redux
 import { useSelector, useDispatch } from "react-redux";
@@ -14,132 +14,128 @@ import {
 // data
 import { imageBaseUrl, productTypesData } from "../data/data";
 
-// toast
-import { toast } from "react-toastify";
+// helpers
+import { errorMessage, getElement, successMessage } from "../helpers/helpers";
 
 // components
-import Product from "../components/Product";
+import Search from "../components/Search";
 import NoData from "../components/NoData";
+import Product from "../components/Product";
 import ConfirmModal from "../components/ConfirmModal";
 import ProductSkeletonLoader from "../components/ProductSkeletonLoader";
 import RecommendationSection from "../components/RecommendationSection";
 
 // images
-import searchImg from "../assets/images/search.svg";
 import reloadImg from "../assets/images/reload.svg";
 import userImg from "../assets/images/edit-user.svg";
 import reviewsImg from "../assets/images/reviews.svg";
 import requestsImg from "../assets/images/requests.svg";
 import productAddImg from "../assets/images/product-add.svg";
 import findProductImg from "../assets/images/find-product.svg";
+import topRightArrowImg from "../assets/images/top-right-arrow.svg";
+
 const Products = () => {
   const dispatch = useDispatch();
-  const isOnline = navigator.onLine;
   const { productType } = useParams();
-  const searchInputRef = useRef(null);
+  const [query, setQuery] = useState("");
   const [loader, setLoader] = useState(false);
+  const [products, setProducts] = useState([]);
   const [loader2, setLoader2] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const [productData, setProductData] = useState([]);
-  const { authData } = useSelector((store) => store.authData);
+  const [currentPageProducts, setCurrentPageProducts] = useState([]);
   const { productsData } = useSelector((store) => store.productsData);
-  const [products, setProducts] = useState([]);
 
   // get products data
   const getProductsData = () => {
     setLoader(true);
+
     // fetch products data
-    axios
-      .get("/Product?userRoleString=2", {
-        headers: {
-          Authorization: "Bearer " + authData.data.token,
-        },
-      })
+    axiosInstance
+      .get("/Product?userRoleString=2")
       .then((res) => {
         dispatch(setProductsData(res.data.allProducts));
-        setProducts(res.data.allProducts);
       })
-      .catch(() => {
-        // error notification
-        if (isOnline) {
-          toast.error("Ma'lumotlarni yuklab bo'lmadi");
-        } else {
-          toast.error("Internet aloqasi mavjud emas!");
-        }
-      })
+      .catch(() => errorMessage.offline("Ma'lumotlarni yuklab bo'lmadi"))
       .finally(() => setLoader(false));
   };
   useEffect(() => {
-    if (productsData.length === 0) {
-      getProductsData();
-    }
+    if (productsData.length === 0) getProductsData();
   }, []);
+
+  // filter products by query
+  const filterProductsByQuery = (q, products) => {
+    if (q && q.length > 0) {
+      const filteredProducts = products.filter((product) =>
+        product.name.toLowerCase().includes(q.toLowerCase())
+      );
+      return filteredProducts;
+    } else {
+      return products;
+    }
+  };
+
+  // set products data with product type
+  useEffect(() => {
+    if (!productType) {
+      setCurrentPageProducts(productsData);
+      setProducts(filterProductsByQuery(query, productsData));
+    } else {
+      const filteredProductsByType = productsData.filter(
+        (product) => product.productType === productType
+      );
+
+      setProducts(filterProductsByQuery(query, filteredProductsByType));
+      setCurrentPageProducts(filteredProductsByType);
+    }
+  }, [productsData, productType]);
+
+  // form submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const input = getElement(event, ".js-search-input");
+    const value = input.value.trim().toLowerCase();
+    setQuery(value);
+    setProducts(filterProductsByQuery(value, currentPageProducts));
+  };
+
+  // clear search input value
+  const clearValue = () => {
+    setProducts(currentPageProducts);
+    setQuery("");
+  };
 
   // delete product
   const deleteProduct = () => {
-    if (isOnline) {
+    if (!loader) {
       setLoader2(true);
 
       // delete product
-      axios
-        .delete("/Product?id=" + productData.productId, {
-          headers: { Authorization: "Bearer " + authData.data.token },
-        })
+      axiosInstance
+        .delete("/Product?id=" + productData.productId)
         .then(() => {
           dispatch(filterProductsData(productData.productId));
-          toast.success("Mahsulot muvafaqiyatli o'chirildi!");
+          successMessage("Mahsulot muvafaqiyatli o'chirildi!");
           setOpenModal(false);
         })
-        .catch(() => toast.error("Nimadir xato ketdi!"))
+        .catch(() => errorMessage.offline("Mahsulotni o'chirib bo'lmadi!"))
         .finally(() => setLoader2(false));
-    } else {
-      toast.error("Internet aloqasi mavjud emas!");
     }
   };
-
-  // search products
-  const handleProductsSearch = (event) => {
-    event.preventDefault();
-    const input = event.target.querySelector(".js-input");
-    const value = input.value.trim().toLowerCase();
-
-    if (value.length > 0) {
-      const filteredProducts = productsData.filter((product) => {
-        return product.brand.toLowerCase().includes(value);
-      });
-
-      setProducts(filteredProducts);
-    } else {
-      productsData.length !== products.length && setProducts(productsData);
-    }
-  };
-
-  // set products data
-  useEffect(() => {
-    if (!productType) {
-      setProducts(productsData);
-    } else {
-      const filteredProductsByType = productsData.filter((product) => {
-        return product.productType === productType;
-      });
-
-      setProducts(filteredProductsByType);
-    }
-  }, [productsData, productType]);
 
   return (
     <>
       {/* page main content */}
       <div className="container pb-12">
         {/* top */}
-        <div className="mb-7">
-          <h1 className="mb-7">Mahsulotlar</h1>
+        <div className="mb-7 space-y-5 sm:space-y-7">
+          <h1>Mahsulotlar</h1>
 
-          {/* product add link */}
-          <div className="grid grid-cols-1 gap-5 mb-7 xs:grid-cols-2 xs:gap-6">
+          {/* product add and product find by id link */}
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
             <Link
               to="/products/product/add"
-              className="flex flex-col items-center gap-3 border-2 border-brand-dark-800 border-dashed rounded-2xl py-5 px-5"
+              className="flex flex-col items-center gap-3 relative border-2 border-brand-dark-800 bg-brand-dark-800/5 rounded-2xl py-10 px-5 group"
             >
               <img
                 width={160}
@@ -148,13 +144,24 @@ const Products = () => {
                 alt="product add image"
                 className="size-28 sm:size-32 md:size-40"
               />
+
               <span className=" font-bold text-center sm:text-xl md:text-2xl">
                 Yangi mahsulot qo'shish
               </span>
+
+              {/* top right arrow image */}
+              <img
+                width={32}
+                height={32}
+                src={topRightArrowImg}
+                alt="top right arrow image"
+                className="absolute top-10 right-10 size-7 opacity-0 transition-all group-hover:opacity-100 group-hover:top-6 group-hover:right-6 xs:size-8"
+              />
             </Link>
+
             <Link
               to="/products/product/find-by-id"
-              className="flex flex-col items-center gap-3 border-2 border-brand-dark-800 border-dashed rounded-2xl py-5 px-5"
+              className="flex flex-col items-center gap-3 relative border-2 border-brand-dark-800 bg-brand-dark-800/5 rounded-2xl py-10 px-5 group"
             >
               <img
                 width={209}
@@ -163,21 +170,31 @@ const Products = () => {
                 alt="find product by id image"
                 className="w-[146px] h-28 sm:w-[167px] sm:h-32 md:w-[209px] md:h-40"
               />
+
               <span className=" font-bold text-center sm:text-xl md:text-2xl">
                 Mahsulotni qidirish
               </span>
+
+              {/* top right arrow image */}
+              <img
+                width={32}
+                height={32}
+                src={topRightArrowImg}
+                alt="top right arrow image"
+                className="absolute top-10 right-10 size-7 opacity-0 transition-all group-hover:opacity-100 group-hover:top-6 group-hover:right-6 xs:size-8"
+              />
             </Link>
           </div>
 
-          {/* nav product types*/}
-          <nav className="overflow-x-auto hidden-scroll p-1 mb-4 bg-brand-dark-800/5 rounded-2xl md:mb-7 xs:p-1.5">
-            <ul className="flex justify-between gap-2 w-full sm:gap-5">
+          {/* nav (product types) */}
+          <nav className="main-nav">
+            <ul className="main-nav-links-wrapper">
               {productTypesData.map((type, index) => {
                 return (
                   <li key={type.id}>
                     <NavLink
                       to={index === 0 ? "/products" : `/products/${type.path}`}
-                      className="inline-block min-w-max p-2.5 rounded-xl text-sm transition-colors duration-300 hover:bg-brand-dark-800 hover:text-brand-creamy-400 xs:text-base sm:px-3 sm:py-2.5"
+                      className="main-nav-link"
                       end
                     >
                       {type.name}
@@ -189,13 +206,10 @@ const Products = () => {
           </nav>
 
           {/* search wrapper */}
-          <div className="flex flex-col-reverse gap-4 md:flex-row">
+          <div className="flex flex-col-reverse gap-6 sm:gap-7 md:flex-row">
             {/* reload btn */}
             <button
-              onClick={() => {
-                !loader && getProductsData(),
-                  (searchInputRef.current.value = "");
-              }}
+              onClick={() => !loader && getProductsData()}
               className="flex items-center justify-center gap-2 shrink-0 bg-brand-dark-800/5 rounded-xl px-5 py-3 xs:py-4"
             >
               <img
@@ -210,32 +224,11 @@ const Products = () => {
 
             {/* search */}
             <div className="w-full">
-              {/* search input wrapper */}
-              <form
-                onSubmit={handleProductsSearch}
-                className="flex items-center relative"
-              >
-                <input
-                  placeholder="Mahsulotlarni qidirish"
-                  name="search"
-                  type="text"
-                  ref={searchInputRef}
-                  className="js-input w-full bg-brand-dark-800/5 rounded-2xl py-2.5 pl-9 pr-24 xs:py-3.5 xs:pl-12 xs:pr-28"
-                />
-
-                <img
-                  width={24}
-                  height={24}
-                  src={searchImg}
-                  alt="search icon"
-                  className="absolute size-5 left-3 xs:left-3.5 xs:size-6"
-                />
-
-                {/* submit btn */}
-                <button className="absolute text-sm right-1 bg-brand-dark-800 text-brand-creamy-400 py-2.5 px-4 rounded-xl xs:text-base xs:right-1.5">
-                  Qidirish
-                </button>
-              </form>
+              <Search
+                action={clearValue}
+                onSubmit={handleSubmit}
+                placeholder="Mahsulotlarni qidirish"
+              />
             </div>
           </div>
         </div>
@@ -267,7 +260,12 @@ const Products = () => {
             </ul>
           )}
 
-          {!loader && products.length === 0 && <NoData />}
+          {!loader && products.length === 0 && (
+            <NoData
+              title="Mahsulotlar topilmadi!"
+              description="Ushbu sahifada hech qanday mahsulot topilmadi."
+            />
+          )}
         </div>
       </div>
 
