bea7c2afa57c4098d535a187a3e6cc9e685a9d84
yaxyobekb <pubgn9642@gmail.com>
2024-05-31 21:01:36 +0500

    add newness page

--- src/pages/Newness.jsx
+++ src/pages/Newness.jsx
@@ -1,165 +1,202 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 // antd
 import "../css/antd.css";
 import { Skeleton } from "antd";
 
+// redux
+import { useSelector } from "react-redux";
+
+// data
+import { imageBaseUrl } from "../data/data";
+
+// axios
+import axiosInstance from "../axios/axiosInstance";
+
+// components
+import NoData from "../components/NoData";
+import RecommendationSection from "../components/RecommendationSection";
+
+// helpers
+import { errorMessage, formatDate, formatTime } from "../helpers/helpers";
+
 // images
 import timeImg from "../assets/images/time.svg";
 import dateImg from "../assets/images/date.svg";
+import newsImg from "../assets/images/news.svg";
 import newsAddImg from "../assets/images/news-add.svg";
-import reviewsImg from "../assets/images/reviews.svg";
-import productsImg from "../assets/images/products.svg";
+import contestsImg from "../assets/images/competition.svg";
 import topRightArrowImg from "../assets/images/top-right-arrow.svg";
+
 const Newness = () => {
   const { newnessId } = useParams();
+  const [loader, setLoader] = useState(true);
+  const [newness, setNewness] = useState(null);
+  const { newsData } = useSelector((state) => state.newsData);
+
+  // get newness data
+  const getNewnessData = () => {
+    axiosInstance
+      .get("News/ById?id=" + newnessId)
+      .then((res) => {
+        if (res.status === 200) {
+          setNewness(res.data);
+        } else {
+          errorMessage();
+        }
+      })
+      .catch(() => {
+        setNewness(null);
+        errorMessage.offline("Ma'lumotlarni yuklab bo'lmadi!");
+      })
+      .finally(() => setLoader(false));
+  };
+
   useEffect(() => {
-    document.title = "Mene Market - " + newnessId;
+    if (newsData && newsData.length > 0) {
+      const findNewness = newsData.find((newness) => newness.id === newnessId);
+      if (findNewness) {
+        setNewness(findNewness);
+        setTimeout(() => setLoader(false), 500);
+      } else {
+        getNewnessData();
+      }
+    } else {
+      getNewnessData();
+    }
   }, []);
+
   return (
     <div className="pb-12">
-      <div className="container">
+      <div className="container !px-0 lg:!px-5">
         <div className="flex items-start flex-col gap-7 lg:flex-row">
           {/* newness */}
+          <div className="w-full px-3 mb-4 xs:px-4 sm:px-5 lg:px-0">
+            {!loader ? (
+              <div className="space-y-4">
+                {newness ? (
+                  <div className="space-y-4">
+                    {/* date & time */}
+                    <div className="flex gap-5">
+                      {/* date */}
+                      <div className="flex items-center gap-1.5 xs:gap-2">
+                        <img
+                          width={20}
+                          height={20}
+                          src={dateImg}
+                          alt="date icon"
+                          className="size-4 xs:size-5 sm:size-6"
+                        />
 
-          <div className="space-y-4">
-            {/* loader */}
-            {
-              // <div className="flex flex-col gap-4">
-              //   <div className="flex items-center justify-between !min-w-0">
-              //     <Skeleton.Input
-              //       active
-              //       className="!w-44 !h-6 xs:!w-52 xs:!h-7"
-              //     />
-              //     <Skeleton.Input
-              //       active
-              //       className="!w-20 !h-10 !rounded-xl sm:!w-[103px] sm:!h-12 "
-              //     />
-              //   </div>
-              //   {/* image */}
-              //   <Skeleton.Image
-              //     active
-              //     className="!min-w-0 !w-full !h-auto !aspect-video !rounded-xl sm:!rounded-2xl"
-              //   />
-              //   {/* title */}
-              //   <div className="flex flex-col gap-1.5">
-              //     <Skeleton.Input
-              //       active
-              //       className="!w-2/3 !h-6 sm:!h-7 lg:!h-8"
-              //     />
-              //     <Skeleton.Input
-              //       active
-              //       className="!w-full !h-6 sm:!h-7 lg:!h-8"
-              //     />
-              //     <Skeleton.Input
-              //       active
-              //       className="!w-full !h-6 sm:!h-7 lg:!h-8"
-              //     />
-              //   </div>
-              //   {/* description */}
-              //   <div className="flex flex-col gap-1.5">
-              //     <Skeleton.Input active className="!w-full !h-5 sm:!h-6" />
-              //     <Skeleton.Input active className="!w-full !h-5 sm:!h-6" />
-              //     <Skeleton.Input active className="!w-full !h-5 sm:!h-6" />
-              //     <Skeleton.Input active className="!w-2/3 !h-6" />
-              //   </div>
-              //   <Skeleton.Input active className="!w-full !h-5 sm:!h-6" />
-              // </div>
-            }
+                        <span className="text-sm xs:text-base sm:text-lg">
+                          {formatDate(newness.postedTime)}
+                        </span>
+                      </div>
 
-            {/* header */}
-            <div className="flex items-center justify-between gap-5">
-              {/* date, time */}
-              <div className="flex gap-5">
-                {/* date */}
-                <div className="flex items-center gap-1.5 xs:gap-2">
-                  <img
-                    width={20}
-                    height={20}
-                    src={dateImg}
-                    alt="date icon"
-                    className="size-4 xs:size-5 sm:size-6"
+                      {/* time */}
+                      <div className="flex items-center gap-1.5 xs:gap-2">
+                        <img
+                          width={20}
+                          height={20}
+                          src={timeImg}
+                          alt="time icon"
+                          className="size-4 xs:size-5 sm:size-6"
+                        />
+
+                        <span className="text-sm xs:text-base sm:text-lg">
+                          {formatTime(newness.postedTime)}
+                        </span>
+                      </div>
+                    </div>
+
+                    {/* content body */}
+                    <div className="grow space-y-4 sm:space-y-5">
+                      <img
+                        width={384}
+                        height={216}
+                        src={imageBaseUrl + newness.imageFilePath}
+                        alt="news image"
+                        className="w-full h-auto bg-brand-dark-800/5 aspect-video object-cover rounded-xl sm:rounded-2xl"
+                      />
+
+                      {/* title */}
+                      <h1 className="text-lg xs:text-xl sm:text-2xl lg:text-3xl">
+                        {newness.name}
+                      </h1>
+
+                      {/* description */}
+                      <pre className="text-base font-fira-sans sm:text-lg">
+                        {newness.description}
+                      </pre>
+                    </div>
+                  </div>
+                ) : (
+                  <NoData
+                    title="Xatolik yuz berdi! Ma'lumotlarni yuklab bo'lmadi."
+                    description=" "
                   />
+                )}
+              </div>
+            ) : (
+              // loader
+              <div className="flex flex-col gap-4">
+                <Skeleton.Input
+                  active
+                  className="!w-44 !h-6 xs:!w-52 xs:!h-7"
+                />
 
-                  <span className="text-sm xs:text-base sm:text-lg">
-                    08.12.2024
-                  </span>
+                {/* image */}
+                <Skeleton.Image
+                  active
+                  className="!min-w-0 !w-full !h-auto !aspect-video !rounded-xl sm:!rounded-2xl"
+                />
+
+                {/* title */}
+                <div className="flex flex-col gap-1.5">
+                  <Skeleton.Input
+                    active
+                    className="!w-2/3 !h-6 sm:!h-7 lg:!h-8"
+                  />
+                  <Skeleton.Input
+                    active
+                    className="!w-full !h-6 sm:!h-7 lg:!h-8"
+                  />
+                  <Skeleton.Input
+                    active
+                    className="!w-full !h-6 sm:!h-7 lg:!h-8"
+                  />
                 </div>
 
-                {/* time */}
-                <div className="flex items-center gap-1.5 xs:gap-2">
-                  <img
-                    width={20}
-                    height={20}
-                    src={timeImg}
-                    alt="time icon"
-                    className="size-4 xs:size-5 sm:size-6"
-                  />
-
-                  <span className="text-sm xs:text-base sm:text-lg">11:59</span>
+                {/* description */}
+                <div className="flex flex-col gap-1.5">
+                  <Skeleton.Input active className="!w-full !h-5 sm:!h-6" />
+                  <Skeleton.Input active className="!w-full !h-5 sm:!h-6" />
+                  <Skeleton.Input active className="!w-full !h-5 sm:!h-6" />
+                  <Skeleton.Input active className="!w-full !h-5 sm:!h-6" />
                 </div>
               </div>
-
-              {/* delete button */}
-              <button className="main-btn  py-2.5 px-3.5 text-sm sm:py-3 sm:px-5 sm:text-base">
-                O'chirish
-              </button>
-            </div>
-
-            {/* main content */}
-            <div className="space-y-4 sm:space-y-5">
-              <img
-                width={384}
-                height={216}
-                src="https://images.unsplash.com/photo-1495020689067-958852a7765e?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3c3xlbnwwfHwwfHx8MA%3D%3D"
-                alt="news image"
-                className="w-full h-auto aspect-video object-cover rounded-xl sm:rounded-2xl"
-              />
-
-              <h1 className="text-lg xs:text-xl sm:text-2xl lg:text-3xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-                recusandae repudiandae. Veritatis ipsa voluptates minus,
-                consectetur animi quos debitis itaque.
-              </h1>
-
-              <p className="text-base sm:text-lg">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et
-                perferendis explicabo veritatis fugiat maxime sed. Fugit modi
-                debitis, consequuntur fugiat laboriosam perferendis quibusdam
-                molestias tempora, unde veniam quaerat cum, impedit aspernatur
-                rem odit dolorem corporis minus. Illum culpa, magni quam saepe
-                rem sint, quos odio, maxime enim reiciendis ad itaque?
-              </p>
-
-              <p className="text-base sm:text-lg">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et
-                perferendis explicabo veritatis fugiat maxime sed. Fugit modi
-                debitis, consequuntur fugiat laboriosam perferendis quibusdam
-                molestias tempora, unde veniam quaerat cum, impedit aspernatur
-                rem odit dolorem corporis minus. Illum culpa, magni quam saepe
-                rem sint, quos odio, maxime enim reiciendis ad itaque?
-              </p>
-            </div>
+            )}
           </div>
 
-          {/* recomendation section */}
-          <section className="w-full lg:min-w-80">
+          {/* recommendation section */}
+          <section className="hidden min-w-80 lg:block">
             <h2 className="mb-7">Ushbu sahifaga oid</h2>
 
-            <ul className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-1">
+            <ul className="space-y-5">
               <li>
                 <Link
                   to="/news/add"
-                  className="flex flex-col items-center justify-center gap-3 relative h-full border-2 border-brand-dark-800 rounded-2xl py-10 px-6 group"
+                  className="flex flex-col items-center justify-center gap-3 relative h-full bg-brand-dark-800/5 border-2 border-brand-dark-800 rounded-2xl py-10 px-6 group"
                 >
                   <img
-                    width={192}
-                    height={192}
+                    width={128}
+                    height={128}
                     src={newsAddImg}
-                    alt="go to find product by id page"
+                    alt="add news image"
                     className="size-28 md:size-32"
                   />
+
                   {/* arrow */}
                   <img
                     width={36}
@@ -168,6 +205,7 @@ const Newness = () => {
                     className="absolute top-10 right-10 size-7 opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:top-5 group-hover:right-5"
                     alt="top right arrow"
                   />
+
                   <h3 className="text-center text-lg xs:text-xl">
                     Yangilik qo'shish
                   </h3>
@@ -176,16 +214,17 @@ const Newness = () => {
 
               <li>
                 <Link
-                  to="/reviews"
-                  className="flex flex-col items-center justify-center gap-3 relative h-full border-2 border-brand-dark-800 rounded-2xl py-10 px-6 group"
+                  to="/news"
+                  className="flex flex-col items-center justify-center gap-3 relative h-full bg-brand-dark-800/5 border-2 border-brand-dark-800 rounded-2xl py-10 px-6 group"
                 >
                   <img
-                    width={192}
-                    height={192}
-                    src={reviewsImg}
-                    alt="go to reviews page"
+                    width={128}
+                    height={128}
+                    src={newsImg}
+                    alt="news image"
                     className="size-28 md:size-32"
                   />
+
                   {/* arrow */}
                   <img
                     width={36}
@@ -194,24 +233,26 @@ const Newness = () => {
                     className="absolute top-10 right-10 size-7 opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:top-5 group-hover:right-5"
                     alt="top right arrow"
                   />
+
                   <h3 className="text-center text-lg xs:text-xl">
-                    Izohlarni boshqarish
+                    Yangiliklar
                   </h3>
                 </Link>
               </li>
 
               <li className="col-span-1 sm:col-span-2 lg:col-span-1">
                 <Link
-                  to="/products"
-                  className="flex flex-col items-center justify-center gap-3 relative h-full border-2 border-brand-dark-800 rounded-2xl py-10 px-6 group"
+                  to="/contests"
+                  className="flex flex-col items-center justify-center gap-3 relative h-full bg-brand-dark-800/5 border-2 border-brand-dark-800 rounded-2xl py-10 px-6 group"
                 >
                   <img
-                    width={192}
-                    height={192}
-                    src={productsImg}
-                    alt="go to products page image"
+                    width={128}
+                    height={128}
+                    src={contestsImg}
+                    alt="contests image"
                     className="size-28 md:size-32"
                   />
+
                   {/* arrow */}
                   <img
                     width={36}
@@ -220,13 +261,44 @@ const Newness = () => {
                     className="absolute top-10 right-10 size-7 opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:top-5 group-hover:right-5"
                     alt="top right arrow"
                   />
-                  <h3 className="text-center text-lg xs:text-xl">
-                    Mahsulotlarni boshqarish
-                  </h3>
+
+                  <h3 className="text-center text-lg xs:text-xl">Contests</h3>
                 </Link>
               </li>
             </ul>
           </section>
+
+          {/* responsive recommendation section */}
+          <div className="block w-full lg:hidden">
+            <RecommendationSection
+              items={[
+                {
+                  title: "Yangilik qo'shish",
+                  path: "news",
+                  image: {
+                    src: newsAddImg,
+                    alt: "add news image",
+                  },
+                },
+                {
+                  title: "Yangiliklar",
+                  path: "news",
+                  image: {
+                    src: newsImg,
+                    alt: "news image",
+                  },
+                },
+                {
+                  title: "Konkurslar",
+                  path: "contests",
+                  image: {
+                    src: contestsImg,
+                    alt: "contests image",
+                  },
+                },
+              ]}
+            />
+          </div>
         </div>
       </div>
     </div>
